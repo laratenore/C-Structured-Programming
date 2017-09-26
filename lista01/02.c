3b0ba3cd7efbad122a5e752c0dcf9ead519a4f35
laratenore <laratenore@hotmail.com>
2017-09-25 22:25:52 -0300

    exercicio 2 da lista01

---
+++ lista01/02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int valor1, valor2;
+    printf("Entre com o primeiro valor:\n");
+    scanf("%d",&valor1);
+    printf("Entre com o segundo valor:\n");
+    scanf("%d",&valor2);
+    
+    printf("A soma dos valores e: %d\n", (valor1+valor2));
+}
